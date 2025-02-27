6ed421c61d4eb49188bad16f5908437057b53c52
hoatd <hoatd@volio.vn>
2025-02-27 15:08:26 +0700

    initial

---
+++ Sources/NavigationLib/NavigationLib.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public class NavigationLib {
+    
+    public init() {}
+    
+    public func ConcatianateStrings(_ string1: String, _ string2: String) -> String {
+        return string1 + string2
+    }
+}
